57843662b9ac5f6b89bb3f99c8e5b03bf383d79f
johnwhh <johnwhh@icloud.com>
2022-03-02 15:09:29 -0500

    Add static image to home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    siteMetadata: {
-      title: `new`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
-}
+  siteMetadata: {
+    title: "Jacks Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};
