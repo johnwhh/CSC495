6920ef4740c9c4d3e0bc67f75e3b2952a57d5f79
johnwhh <johnwhh@icloud.com>
2022-04-08 14:38:00 -0400

    Add ability to browse

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This is a simple Gatsby demo consuming the popular RESTful Pokémon API called P
 ### Home
 The landing page is simple and suggests that the user explore the site.
 ### Pokedex
-The Pokedex is a simple search based page. The user can enter the name of any Pokemon and the site will present its picture and interesting information about it.
+The Pokedex is a simple search based page. The user can enter the name of any Pokemon and the site will present its picture and interesting information about it. Once a Pokemon is searched for, two buttons appear that allow the user to browse between the Pokemon.
 ### About
 The about page describes the site and its creator.
--- src/pages/pokedex.js
+++ src/pages/pokedex.js
@@ -3,18 +3,28 @@ import Layout from '../components/layout'
 import Pokemon from "../components/pokemon";
 import useEndpoint from "../hooks/useEndpoint"
 
+const getNextId = (currentPokemon) => {
+  return (currentPokemon.id % 898) + 1
+}
+
+const getPreviousId = (currentPokemon) => {
+  return currentPokemon.id <= 1 ? 898 : currentPokemon.id - 1
+}
+
 const PokedexPage = () => {
   const {data, setData} = useEndpoint();
   return (
     <Layout pageTitle="Pokédex">
       <p>Welcome to my Pokédex.</p>
       <main>
+        {data.result != null ? <button onClick={() => setData({...data, slug: getPreviousId(data.result)})}>Previous</button> : null}
         <input
           type="search"
           placeholder="Enter a pokemon..."
           value={data.slug}
           onChange={(e) => setData({...data, slug: e.target.value})}
         />
+        {data.result != null ? <button onClick={() => setData({...data, slug: getNextId(data.result)})}>Next</button> : null}
         <br />
         {data.result != null ? <Pokemon pokemon={data.result} /> : null}
       </main>
